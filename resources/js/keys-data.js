389993bed0c294de723be3a6ab6818c990cd8cea
Myroslav Oleinykov <m@meeroslaph.name>
2015-12-19 14:10:58 +0200

    All the data stored in HTML is converted to use Handlebars templates.

--- resources/js/keys-data.js
+++ resources/js/keys-data.js
@@ -1,8 +1,11 @@
 var keys_data = {
-  category : [
-    {
-      name : "Numrow",
-      keys : [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    }
-  ]
+  numrow : [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+  first_row : ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  second_row : ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  third_row : ["Z", "X", "C", "V", "B", "N", "M"],
+  fourth_row : ["space"],
+  first_numpad_row : [7, 8, 9],
+  second_numpad_row : [4, 5, 6],
+  third_numpad_row : [1, 2, 3],
+  fourth_numpad_row : [0]
 };
--- resources/js/logic.js
+++ resources/js/logic.js
@@ -3,6 +3,46 @@ var numrow_template = Handlebars.compile(source);
 var html = numrow_template(keys_data);
 $(".numrow").html(html);
 
+source = $("#first-row-template").html();
+var first_row_template = Handlebars.compile(source);
+html = first_row_template(keys_data);
+$("div[name='first-row']").html(html);
+
+source = $("#first-numpad-row-template").html();
+var first_numpad_row_template = Handlebars.compile(source);
+html = first_numpad_row_template(keys_data);
+$("div[name='first-numpad-row']").html(html);
+
+source = $("#second-row-template").html();
+var second_row_template = Handlebars.compile(source);
+html = second_row_template(keys_data);
+$("div[name='second-row']").html(html);
+
+source = $("#second-numpad-row-template").html();
+var second_numpad_row_template = Handlebars.compile(source);
+html = second_numpad_row_template(keys_data);
+$("div[name='second-numpad-row']").html(html);
+
+source = $("#third-row-template").html();
+var third_row_template = Handlebars.compile(source);
+html = third_row_template(keys_data);
+$("div[name='third-row']").html(html);
+
+source = $("#third-numpad-row-template").html();
+var third_numpad_row_template = Handlebars.compile(source);
+html = third_numpad_row_template(keys_data);
+$("div[name='third-numpad-row']").html(html);
+
+source = $("#fourth-row-template").html();
+var fourth_row_template = Handlebars.compile(source);
+html = fourth_row_template(keys_data);
+$("div[name='fourth-row']").html(html);
+
+source = $("#fourth-numpad-row-template").html();
+var fourth_numpad_row_template = Handlebars.compile(source);
+html = fourth_numpad_row_template(keys_data);
+$("div[name='fourth-numpad-row']").html(html);
+
 var button = $("button");
 button.each(function () {
   if ($(this).attr("name") != "space") {
